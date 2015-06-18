78925b4889b7195b8556e0bb1a463446e58e18c9
fixiedude17 <aaron@degree9.io>
2015-06-18 13:37:15 -0600

    Primary DNS Setup

---
+++ PrimaryDNS.ps1
@@ -0,0 +1,17 @@
+configuration Sample_xDnsServerZoneTransfer_TransferToAnyServer
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$DnsZoneName,
+
+        [Parameter(Mandatory)]
+        [String]$TransferType
+    )
+    Import-DscResource -module xDnsServer
+    xDnsServerZoneTransfer TransferToAnyServer
+    {
+        Name = $DnsZoneName
+        Type = $TransferType
+    }
+}
